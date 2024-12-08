30e3436be83780a68b20c02b91d80ed9127aab3e
gitYoungren <youngrengitonga@gmail.com>
2024-12-08 23:22:10 +0300

    changes made in magazine.py

--- lib/magazine.py
+++ lib/magazine.py
@@ -1,28 +1,41 @@
-
 class Magazine:
     def __init__(self, name, category):
-        self.name = name  
-        self.category = category
-
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        
+        if not isinstance(category, str):
+            raise ValueError("Category must be a string")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        
+        self._name = name
+        self._category = category
+        self._articles = []
+    
     @property
     def name(self):
         return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            raise TypeError("Name must be a string")
-        if len(value) < 2 or len(value) > 16:
-            raise ValueError("Name must be between 2 and 16 characters")
-        self._name = value
+    
     @property
     def category(self):
         return self._category
-
+    
     @category.setter
-    def category(self, value):
-        if not isinstance(value, str):
-            raise TypeError("Category must be a string")
-        if len(value) == 0:
+    def category(self, new_category):
+        if not isinstance(new_category, str):
+            raise ValueError("Category must be a string")
+        if len(new_category) == 0:
             raise ValueError("Category must be longer than 0 characters")
-        self._category = value 
+        self._category = new_category
+    
+    def articles(self):
+        return self._articles
+    
+    def contributors(self):
+        return list(set(article.author for article in self._articles))
+    
+    def publish_article(self, article):
+        if isinstance(article, Article):
+            self._articles.append(article)
